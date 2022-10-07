40a3f36a80a2662dad919edd47a66331b237d223
Nisitha-kode <107039273+Nisitha-kode@users.noreply.github.com>
2022-10-07 17:35:42 +0200

    Update Function5.js

--- Function5.js
+++ Function5.js
@@ -2,9 +2,9 @@
  *               So that instead of saying for example: console.log(parameters[0]) you can just say console.log(text1)
  */
 //function that takes 2 parameters with default values
-function greeting (...parameters) {
+function greeting (...args) {
   //Destructure the parameters using array
-  let [text1, text2] = parameters
+  let [text1, text2] = args
   console.log(text1, text2)
 }
 //Function call with parameters
