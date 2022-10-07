2a35f0a14dffe3bc2f3489ad717a58ca00a4783e
Nisitha-kode <107039273+Nisitha-kode@users.noreply.github.com>
2022-10-07 11:16:03 +0200

    Update Function27.js

--- Function27.js
+++ Function27.js
@@ -5,12 +5,15 @@
  */
 // Write your solution (code + comments + tests) below here:
 
+import arrays from './lib.js'
+const norwegianArrays = arrays()
+
 let team1 = new Array(6)
 team1.fill(null)
 team1.forEach((value, index) => {
   console.log(value, index)
 })
-/*
+
 team1[index] = randomPerson()
-team1 = team1.map((value) => randomPerson())
-console.log(team1)*/
+team1 = team1.map(value => randomPerson())
+console.log(team1)
